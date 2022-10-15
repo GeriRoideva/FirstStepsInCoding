a87987b1625219a667285970f65f3ef9aa8dc3a7
GeriRoideva <115742620+GeriRoideva@users.noreply.github.com>
2022-10-15 15:52:23 +0300

    Add files via upload

---
+++ TrueOrFalse.java
@@ -0,0 +1,12 @@
+package FirstStepsInCoding;
+
+public class TrueOrFalse {
+    public static void main(String[] args) {
+
+        String name = "Geri";
+
+        boolean result = name.equals("geri");
+        System.out.println(result);
+
+    }
+}
