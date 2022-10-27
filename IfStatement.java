83ef0e950acd223570add2377dd2e031ba350051
GeriRoideva <115742620+GeriRoideva@users.noreply.github.com>
2022-10-27 18:42:41 +0300

    Add files via upload

---
+++ IfStatement.java
@@ -0,0 +1,15 @@
+package FirstStepsInCoding;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        int a = 10;
+
+        if (a>8){
+            System.out.println("This is true!");
+        }
+
+        if (a<8){
+            System.out.println("This is incorrect");
+        }
+    }
+}
